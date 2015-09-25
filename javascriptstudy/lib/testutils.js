1f3e936066b0e1eae0ca73b638c414b65b502495
GZW04_000 <GZW04_000@eiji-newpc>
2015-09-25 18:43:31 +0900

    update.

---
+++ javascriptstudy/lib/testutils.js
@@ -0,0 +1,98 @@
+/**自作のログ出力ユーティリティ。*/
+
+//**名前空間**//
+var testutils;
+if(!testutils){
+	testutils = {};
+}
+
+/**
+  * minukuba_utils.logger
+  *
+  * minukuba_utils.logger.setLevel(2);
+  * minukuba_utils.logger.log("message");
+  * minukuba_utils.logger.log("message",1)
+  * minukuba_utils.logger.log("message",2)
+  * minukuba_utils.logger.log("message",3)
+  *
+  * 出力先拡張例
+  * minukuba_utils.logger.write = function(message){
+  *     $('#log-message-ol').append('<li>'+message+'</li>');
+  * }
+  *
+  **/
+(function (testutils) {
+	var Logger = function(){
+		this.level = 1;
+	};
+
+	Logger.prototype.setLevel = function(level) {
+		this.level = level || 1;
+	}
+
+	Logger.prototype.log = function(message,level) {
+		level = level || 1;
+		if(this.level >= level){
+			this.write(message);
+		}
+	}
+
+	Logger.prototype.write = function(message){
+		console.log(message);
+	}
+
+	testutils.logger = new Logger();
+
+}(testutils));
+
+/**
+ * qUnit Fixtureの代替を実現するクラス その２ ajaxで外部HTMLを読み込めるようにした。。 <div
+ * id='<#qunit-fixture'>を使用した場合、対象のdivはブラウザに表示されずわかりづらい。
+ * そのため、上記以外のdivを設け、対象のdivのテスト毎初期化処理を行う。
+ * **TODO コメント追加**
+ */
+(function(testutils){
+	var HtmlFixture = function(){
+		this.savedHtml = '';
+		this.id = '#custom-fixture';
+	}
+
+	HtmlFixture.prototype.setupFixture = function storeFixture() {
+		var fixture = $(this.id);
+		if ( fixture ) {
+			this.savedHtml = fixture.html();
+		}
+		testutils.logger.log("setupFixture:"+this.savedHtml)
+	}
+
+	HtmlFixture.prototype.replaceFixture = function replaceFixture(replaceHtml) {
+		if(replaceHtml != undefined && replaceHtml != null){
+			$.ajax(
+				{
+					async : false,
+					type : 'GET',
+					url : replaceHtml,
+					dataType : 'html',
+					success  : function(data){
+						testutils.logger.log("replaceFixture:"+data)
+						$("#custom-fixture").html(data);
+					},
+					error : function(xhr, textStatus, errorThrown){
+						testutils.logger.log(textStatus);
+						testutils.logger.log(errorThrown);
+					}
+				}
+			);
+		}
+	}
+
+	HtmlFixture.prototype.teardownFixture = function restoreFixture(){
+		var fixture = $(this.id);
+		if ( fixture ) {
+			fixture.html(this.savedHtml);
+		}
+		testutils.logger.log("teardownFixture:"+this.savedHtml)
+	}
+
+	testutils.htmlfixture = new HtmlFixture();
+}(testutils));
--- javascriptstudy/ui/tests-qunit-fixture-example.js
+++ javascriptstudy/ui/tests-qunit-fixture-example.js
@@ -1,89 +1,39 @@
 "use strict";
 
-/**
- * qUnit Fixtureの代替を実現するクラス その２ ajaxで外部HTMLを読み込めるようにした。。 <div
- * id='<#qunit-fixture'>を使用した場合、対象のdivはブラウザに表示されずわかりづらい。
- * そのため、上記以外のdivを設け、対象のdivのテスト毎初期化処理を行う。
- */
-var Fixture = (function(){
-	var fixtureHtml;
-
-	var Fixture = function(){
-	}
-
-	Fixture.prototype.setupFixture = function storeFixture() {
-		var fixture = $("#custom-fixture" );
-		if ( fixture ) {
-			fixtureHtml = fixture.html();
-		}
-		alert("setupFixture:"+fixtureHtml)
-	}
-
-	Fixture.prototype.replaceFixture = function replaceFixture(replaceHtml) {
-		if(replaceHtml != undefined && replaceHtml != null){
-			$.ajax(
-				{
-					async : false,
-					type : 'GET',
-					url : replaceHtml,
-					dataType : 'html',
-					success  : function(data){
-						alert("replaceFixture:"+data)
-						$("#custom-fixture").html(data);
-					},
-					error : function(xhr, textStatus, errorThrown){
-						alert(textStatus);
-						alert(errorThrown);
-					}
-				}
-			);
-		}
-	}
-
-	Fixture.prototype.teardownFixture = function restoreFixture(){
-		var fixture = $("#custom-fixture" );
-		if ( fixture ) {
-			alert("teardownFixture:"+fixtureHtml)
-			fixture.html(fixtureHtml);
-		}
-	}
-	return new Fixture();
-}());
-
-// 1 テストのグルーピング
+//1 テストのグルーピング
 module("UIテストサンプル", {
-  // 2 セットアップ
-  beforeEach:function () {
-	  // Loggerのwrite処理を起きかえ
-	  minokuba_utils.logger.write = function(message){
-		  $('#log-message-ol').append('<li>'+message+'</li>');
-	  };
+	// 2 セットアップ
+	beforeEach:function () {
+		// Loggerのwrite処理を起きかえ
+		testutils.logger.write = function(message){
+			$('#log-message-ol').append('<li>'+message+'</li>');
+		};
 
-	  // #custom-fixtureのストア処理
-	  Fixture.setupFixture();
+		// #custom-fixtureのストア処理
+		testutils.htmlfixture.setupFixture();
 
-  },
-  afterEach:function() {
-	  // #custom-fixtureのリストア処理
-	  Fixture.teardownFixture();
+	},
+	afterEach:function() {
+		// #custom-fixtureのリストア処理
+		testutils.htmlfixture.teardownFixture();
 
-	  // Logger出力エリアは、必要ならクリアできる
-	  $('#log-message-ol').empty();
-  }
+		// Logger出力エリアは、必要ならクリアできる
+//		$('#log-message-ol').empty();
+	}
 });
 
 
 
-// 3 テストケース定義
+//3 テストケース定義
 test("UIテストサンプル", function () {
-	  minokuba_utils.logger.log("test1");
+	testutils.logger.log("test1");
 
-	Fixture.replaceFixture('./html/html-test01.html');
+	testutils.htmlfixture.replaceFixture('./html/html-test01.html');
 	ok(true);
 });
 
 //3 テストケース定義
 test("UIテストサンプル2", function () {
-	  minokuba_utils.logger.log("test2");
+	testutils.logger.log("test2");
 	ok(true);
 });
